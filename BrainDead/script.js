2fd8a5cf9dfe2719f2fecf7a2200609740b841b3
cyber0knight <issacnewtonmukara@gmail.com>
2024-04-02 11:16:18 +0530

    update braindead mys

--- BrainDead/script.js
+++ BrainDead/script.js
@@ -14,16 +14,3 @@ function openTab(event, tabname) {
   event.currentTarget.classList.add("active-link");
   document.getElementById(tabname).style.display = "block"; // Display only the selected tab content
 }
-
-  // // Function to change text color dynamically //
-  // function changeTextColor() {
-  //   var paragraphs = document.querySelectorAll(".dynamic-text");
-  //   paragraphs.forEach(function(paragraph) {
-  //     var colors = ['#ff0000', '#00ff00', '#0000ff']; // Array of colors
-  //     var randomColor = colors[Math.floor(Math.random() * colors.length)]; // Randomly select a color
-  //     paragraph.style.color = randomColor; // Apply the color to the paragraph
-  //   });
-  // }
-
-  // // Change text color every 3 seconds
-  // setInterval(changeTextColor, 3000);
